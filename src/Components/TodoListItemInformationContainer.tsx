a2f5ae50a3ee8a2b456decc7fa9ffbbf4ab5b094
0CottonBuds <emiljohnllanes772@gmail.com>
2024-01-07 21:56:38 +0800

    structure TodoListItemInformationContainer

--- src/Components/TodoListItemInformationContainer.tsx
+++ src/Components/TodoListItemInformationContainer.tsx
@@ -6,10 +6,13 @@ interface TodoListItemInformationContainerProps {
 
 export default function TodoListItemInformationContainer({
   currentItemSelectedIndex,
+  Todos,
 }: TodoListItemInformationContainerProps) {
   return (
     <div className="todo-list-item-infomarion-container">
-      {currentItemSelectedIndex}
+      <h1>Title: {Todos[currentItemSelectedIndex].title}</h1>
+      <h3>Description</h3>
+      <p>{Todos[currentItemSelectedIndex].description}</p>
     </div>
   );
 }
