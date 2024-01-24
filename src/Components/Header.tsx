5a28a70e0f69cf5b7da1d47c361ad640f4401ca8
0CottonBuds <emiljohnllanes772@gmail.com>
2024-01-24 23:19:35 +0800

    add styling to header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,4 +1,13 @@
 // Header/ nav bar/ logo
+import reactLogo from "../assets/react.png";
+
 export default function Header() {
-  return <header className="heading">To-React</header>;
+  return (
+    <header className="heading">
+      <div className="heading-logo-container">
+        <img className="heading-logo" src={reactLogo}></img>
+      </div>
+      <p className="heading-title">To-React</p>
+    </header>
+  );
 }
